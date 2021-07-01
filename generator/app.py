fd544fff965cf5b93e02ae2bb32093e80286c96f
Jordan <jtang0819@gmail.com>
2021-07-01 15:16:17 -0400

    update 3pm

--- generator/app.py
+++ generator/app.py
@@ -1,1 +1,17 @@
+import os
+from time import sleep
+from transactions import create_random_transaction
+import json
 from kafka import KafkaProducer
+
+KAFKA_BROKER_URL = os.environ.get("KAFKA_BROKER_URL")
+producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL)
+
+if __name__ == "__main__":
+    producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL)
+    while True:
+        transaction: dict = create_random_transaction()
+        message: str = json.dumps(transaction)
+        producer.send("queuing.transactions", value=message.encode())
+        sleep(1)
+
